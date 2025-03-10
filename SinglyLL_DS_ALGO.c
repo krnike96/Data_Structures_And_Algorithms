512e84bfbc10649aa1e5e050e168259888201a13
Niket Kumar <krnike96@gmail.com>
2025-03-10 15:59:54 +0530

    SinglyLL all  Basic Operations Done!

--- SinglyLL_DS_ALGO.c
+++ SinglyLL_DS_ALGO.c
@@ -1,19 +1,19 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-struct node{
+typedef struct node{
     int data;
     struct node * next;
-};
+}node;
 
-struct node * head = NULL;
+node * head = NULL;
 
-struct node * createNode(){
-    return (struct node *)malloc(sizeof(struct node));
+node * createNode(){
+    return (node *)malloc(sizeof(node));
 }
 
 void insertFirst(int emt){
-    struct node * temp = createNode();
+    node * temp = createNode();
     temp->data = emt;
     temp->next = head;
     head = temp;
@@ -24,11 +24,11 @@ void insertLast(int emt){
         insertFirst(emt);
         return;
     }
-    struct node * temp = head;
+    node * temp = head;
     while(temp->next != NULL){
         temp = temp->next;
     }
-    struct node * nn = createNode();
+    node * nn = createNode();
     nn->data = emt;
     nn->next = NULL;
     temp->next = nn;
@@ -44,7 +44,7 @@ void insertPos(int pos,int emt){
         return;
     }
     int nCount = 0;
-    struct node * temp = head;
+    node * temp = head;
     while(temp != NULL){
         nCount++;
         temp = temp->next;
@@ -57,21 +57,183 @@ void insertPos(int pos,int emt){
     for(int i = 0;i < pos - 2; i++){ // pos - 2 means first head is skipped as temp is already at head and
         temp = temp->next;           // i've to traverse till the position before the required position to
     }                                // link the new node with the previous node
-    struct node * nn = createNode();
+    node * nn = createNode();
     nn->next = temp->next;
     nn->data = emt;
     temp->next = nn;
+}
+
+void deleteFirst(){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    node * temp = head;
+    head = temp->next;
+    free(temp);
+    temp = NULL;
+}
+
+void deleteLast(){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    node * temp = head;
+    if(temp->next == NULL){
+        deleteFirst();
+        return;
+    }
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    
+    free(temp->next);
+    temp->next = NULL;
     
 }
 
-void display(){
+void deletePos(int pos){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    if(pos <= 0){
+        printf("\nNot a Valid Position.");
+        return;
+    }
+    if(pos == 1){
+        deleteFirst();
+        return;
+    }
+    int nCount = 0;
+    node * temp = head;
+    while(temp != NULL){
+        nCount++;
+        temp = temp->next;
+    }
+    if(pos > nCount){
+        printf("\nNot a Valid Position.");
+        return;
+    }
+    if(pos == nCount){
+        deleteLast();
+        return;
+    }
+    temp = head;
+    for(int i = 0; i < pos - 2; i++){   // pos - 2 means first head is skipped as temp is already at head and
+        temp=temp->next;                // i've to traverse till the position before the required position to
+    }                                   // delete the node at this position
+    node * temp1 = temp->next;
+    temp->next = temp->next->next;
+    free(temp1);
+    temp1 = NULL;
+}
+
+void updateFirst(int emt){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    head->data = emt;
+}
+
+void updateLast(int emt){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    node * temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->data = emt;
+}
+
+void updatePos(int pos,int emt){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    if(pos <= 0){
+        printf("\nNot a Valid Position.");
+        return;
+    }
+    if(pos == 1){
+        updateFirst(emt);
+        return;
+    }
+    int nCount = 0;
+    node * temp = head;
+    while(temp != NULL){
+        nCount++;
+        temp = temp->next;
+    }
+    if(pos > nCount){
+        printf("\nNot a Valid Position.");
+        return;
+    }
+    if(pos == nCount){
+        updateLast(emt);
+        return;
+    }
+    temp = head;
+    for(int i = 0; i < pos - 1; i++){   // pos - 1 means first head is skipped as temp is already at head and
+        temp=temp->next;                // i've to traverse till the position that is to be updated!
+    }                                  
+    temp->data = emt;
+    return;
+}
+
+void search(int key){
+    if(head == NULL){
+        printf("The List is Empty!\n");
+        return;
+    }
+    int nCount = 0;
+    node * temp = head;
+    while(temp->next != NULL){
+        nCount++;
+        if(temp->data == key){
+            printf("The Key Element Found at Position : %d",nCount);
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("Key Element not Found!");
+    return;
+}
+
+void bubbleSort(){
+    if(head == NULL || head->next == NULL){
+        printf("The List is Empty or Single Element!\n");
+        return;
+    }
+    node * temp;
+    node * end = NULL;
+    int swap;
+    do{
+        swap = 0;
+        temp = head;
+        while(temp->next != end){      //running the loop till it reaches the end oof the list
+            if(temp->data > temp->next->data){
+                temp->data ^= temp->next->data ^= temp->data ^= temp->next->data;
+                swap = 1;
+            }
+            temp = temp->next;
+        }
+        end = temp; //pointing the end to one step backwards as the largest element is at the end 
+                    //so no need to check that element
+    }while(swap);
+}
+void display(){ 
     int nCount = 0;
     if(head == NULL){
-        printf("\nThe List is Empty!");
+        printf("The List is Empty!\n");
         return;
     }else{
         printf("\nThe List is : ");
-        struct node * temp = head;
+       node * temp = head;
     while(temp != NULL){
         nCount++;
         printf("%d->",temp->data);
@@ -85,18 +247,19 @@ void display(){
 int main(){
     int choice,emt,pos;
     while(1){
+        printf("\nSingly LL");
         printf("\n1.Insert at First");
         printf("\n2.Insert at Last");
         printf("\n3.Insert at Specific Position");
         printf("\n4.Delete at First");
-        printf("\n5.Delete at First");
-        printf("\n6.Delete at Last");
-        printf("\n7.Insert at Specific Position");
-        printf("\n8.Update at First");
-        printf("\n9.Update at Last");
-        printf("\n10.Update at Specific Position");
-        printf("\n11.Sort");
-        printf("\n12.Search");
+        printf("\n5.Delete at Last");
+        printf("\n6.Delete at Specific Position");
+        printf("\n7.Update at First");
+        printf("\n8.Update at Last");
+        printf("\n9.Update at Specific Position");
+        printf("\n10.Sort(Elemental)");
+        printf("\n11.Search(Linear)");
+        printf("\n12.Display List");
         printf("\n0.Exit");
         printf("\nEnter Your Choice : ");
         scanf("%d",&choice);
@@ -119,6 +282,52 @@ int main(){
                 printf("Enter the Element : ");
                 scanf("%d",&emt);
                 insertPos(pos,emt);
+                display();
+                break;
+            case 4:
+                deleteFirst();
+                display();
+                break;
+            case 5:
+                deleteLast();
+                display();
+                break;
+            case 6:
+                printf("Enter the Position : ");
+                scanf("%d",&pos);
+                deletePos(pos);
+                display();
+                break;
+            case 7:
+                printf("Enter the Element : ");
+                scanf("%d",&emt);
+                updateFirst(emt);
+                display();
+                break;
+            case 8:
+                printf("Enter the Element : ");
+                scanf("%d",&emt);
+                updateLast(emt);
+                display();
+                break;
+            case 9: 
+                printf("Enter the Position : ");
+                scanf("%d",&pos);
+                printf("Enter the Element : ");
+                scanf("%d",&emt);
+                updatePos(pos,emt);
+                display();
+                break;
+            case 10:
+                bubbleSort();
+                display();
+                break;
+            case 11:
+                printf("Enter the Key Element : ");
+                scanf("%d",&emt);
+                search(emt);
+                break;
+            case 12:
                 display();
                 break;
             case 0:

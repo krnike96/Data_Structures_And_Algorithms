d6cf67e4cb7f9213ddfc969923bfdf1686ef74a2
Niket Kumar <krnike96@gmail.com>
2025-03-13 14:53:50 +0530

    SingleCircularLL Insert At First & Deletion Operation Done!

--- Linked_Lists/SingleCircularLL_DS_ALGO.cpp
+++ Linked_Lists/SingleCircularLL_DS_ALGO.cpp
@@ -3,104 +3,143 @@
 
 using namespace std;
 
+int nCount = 0;
+
 namespace SCLL
 {
-    struct data
-    {
-        string name;
-        int age;
-    };
-    struct node
-    {
-        data user;
-        node *next;
-    };
-    node *createNode()
-    {
-        node *nn = new (std::nothrow) node;
-        if (!nn)
-        {
-            std::cerr << "Memory allocation failed!" << std::endl; // Handle the error
-        }
-        else
-            return nn;
-    }
+     struct data
+     {
+          string name;
+          int age;
+     };
+     struct node
+     {
+          data *user;
+          node *next;
+     };
+     SCLL::node *createNode()
+     {
+          node *nn = new (std::nothrow) node;
+          if (!nn)
+          {
+               std::cerr << "Memory allocation failed!" << std::endl; // Handle the error
+               return nullptr;
+          }
+          nn->user = new (std::nothrow) data;
+          if(!nn->user){
+               std::cerr <<"Memory Allocation Failed" << std::endl;
+               return nullptr;
+          }
+          return nn;
+     }
 }
 
-void insertFirst(node **head, string name, int age, int sly);
-void insertLast(node **head, string name, int age, int sly);
-void insertPos(node **head, string name, int age, int sly, int pos);
-void deleteFirst(node **head, string name, int age, int sly);
-void deleteLast(node **head, string name, int age, int sly);
-void deletePos(node **head, string name, int age, int sly, int pos);
-void updateFirst(node **head, string name, int age, int sly);
-void updateLast(node **head, string name, int age, int sly);
-void updatePos(node **head, string name, int age, int sly, int pos);
-void sort(node **head);
-void search(node **head, string name); // Function Overloading
-void search(node **head, int age);     // Function Overloading
-void search(node **head, int sly);     // Function OVerloading
-void display(node **head);
-void displayReverse(node **head);
+void insertFirst(SCLL::node **head, string name, int age)
+{
+     SCLL::node *nn = SCLL::createNode();
+     nn->user->name = name;
+     nn->user->age = age;
+     if(*head == nullptr){
+          nn->next = nn;
+     }else{
+          SCLL::node *LastNode = *head;
+          while(LastNode->next != *head){
+               LastNode = LastNode->next;
+          }
+          nn->next = *head;
+          LastNode->next = nn;
+     }
+     *head = nn;
+     nCount++;
+     return;
+}
+// void insertLast(node **head, string name, int age, int sly);
+// void insertPos(node **head, string name, int age, int sly, int pos);
+// void deleteFirst(node **head, string name, int age, int sly);
+// void deleteLast(node **head, string name, int age, int sly);
+// void deletePos(node **head, string name, int age, int sly, int pos);
+// void updateFirst(node **head, string name, int age, int sly);
+// void updateLast(node **head, string name, int age, int sly);
+// void updatePos(node **head, string name, int age, int sly, int pos);
+// void sort(node **head);
+// void search(node **head, string name); // Function Overloading
+// void search(node **head, int age);     // Function Overloading
+// void search(node **head, int sly);     // Function OVerloading
+void display(SCLL::node **head){
+     if(*head == nullptr){
+          cout<<"The List is Empty!"<<endl;
+          return;
+     }
+     SCLL::node *temp = *head;
+     cout<<endl<<"The List is : ";
+     while(true){
+          cout<<"("<<temp->user->name<<"-"<<temp->user->age<<")->";
+          temp = temp->next;
+          if(temp == *head) break;
+     }
+     cout<<"Head"<<endl;
+     cout<<"Nodes : "<<nCount<<endl;
+}
+// void displayReverse(node **head);
 
 int main()
 {
-    SCLL::node *head{nullptr};
-    int choice, pos, age, sly;
-    string name;
-    cout << endl
-         << "Single Circular LL";
-    while (1)
-    {
-        cout << endl
-             << "1.Insert at First";
-        cout << endl
-             << "2.Insert at Last";
-        cout << endl
-             << "3.Insert at Specific Position";
-        cout << endl
-             << "4.Insert at First";
-        cout << endl
-             << "5.Insert at Last";
-        cout << endl
-             << "6.Insert at Specific Position";
-        cout << endl
-             << "7.Insert at First";
-        cout << endl
-             << "8.Insert at Last";
-        cout << endl
-             << "9.Insert at Specific Position";
-        cout << endl
-             << "10.Sort(Elemental)";
-        cout << endl
-             << "11.Search(Linear)";
-        cout << endl
-             << "12.Display";
-        cout << endl
-             << "0.Exit";
-        cout << endl
-             << "Enter Your Choice : ";
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            cout << endl
-                 << "Enter the User's Name : ";
-            getline(cin, name);
-            cout << endl
-                 << "Enter the User's Age : ";
-            cin >> age;
-            cout << endl
-                 << "Enter the User's Salary : ";
-            cin >> sly;
-            insertFirst(&head, name, age, sly);
-            display(&head);
-            break;
-        default:
-            cout << endl
-                 << "Error - 400 : Bad Behaviour";
-            break;
-        }
-    }
-    return 0;
+     SCLL::node *head{nullptr};
+     int choice, pos, age;
+     string name;
+     cout << endl
+          << "Single Circular LL";
+     while (1)
+     {
+          cout << endl
+               << "1.Insert at First";
+          cout << endl
+               << "2.Insert at Last";
+          cout << endl
+               << "3.Insert at Specific Position";
+          cout << endl
+               << "4.Insert at First";
+          cout << endl
+               << "5.Insert at Last";
+          cout << endl
+               << "6.Insert at Specific Position";
+          cout << endl
+               << "7.Insert at First";
+          cout << endl
+               << "8.Insert at Last";
+          cout << endl
+               << "9.Insert at Specific Position";
+          cout << endl
+               << "10.Sort(Elemental)";
+          cout << endl
+               << "11.Search(Linear)";
+          cout << endl
+               << "12.Display";
+          cout << endl
+               << "0.Exit";
+          cout << endl
+               << "Enter Your Choice : ";
+          cin >> choice;
+          switch (choice)
+          {
+          case 1:
+               cout << endl
+                    << "Enter the User's Name : ";
+               cin>>name;
+               cout << endl
+                    << "Enter the User's Age : ";
+               cin >> age;
+               insertFirst(&head, name, age);
+               display(&head);
+               break;
+          case 0:
+               cout << "Exiting...";
+               return 0;
+          default:
+               cout << endl
+                    << "Error - 400 : Bad Behaviour";
+               break;
+          }
+     }
+     return 0;
 }

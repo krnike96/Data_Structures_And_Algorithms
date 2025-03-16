bdd5b21ce27ef20f24fe1088ca2f2634b4266017
Niket Kumar <krnike96@gmail.com>
2025-03-16 23:40:58 +0530

    DoubleCircularLL Deletion Operations Done & Add Auto File Input

--- Linked_Lists/DoubleCircularLL_DS_ALGO.cpp
+++ Linked_Lists/DoubleCircularLL_DS_ALGO.cpp
@@ -1,6 +1,7 @@
-//Head & Tail Approach
+// Head & Tail Approach
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -37,27 +38,33 @@ namespace DCLL
      }
 }
 
-void insertFirst(DCLL::node **head,DCLL::node **tail, string name, int age){
-     DCLL::node * nn = DCLL::createNode();
+void insertFirst(DCLL::node **head, DCLL::node **tail, string name, int age)
+{
+     DCLL::node *nn = DCLL::createNode();
      nn->user->name = name;
      nn->user->age = age;
-     if(*head == nullptr){
-         nn->next = nn;
-         nn->prev = nn;
-         *head = nn;
-         *tail = nn;
-     }else{
-         nn->next = *head;
-         (*head)->prev = nn;
-         nn->prev = *tail;
+     if (*head == nullptr)
+     {
+          nn->next = nn;
+          nn->prev = nn;
+          *head = nn;
+          *tail = nn;
+     }
+     else
+     {
+          nn->next = *head;
+          (*head)->prev = nn;
+          nn->prev = *tail;
           (*tail)->next = nn;
           *head = nn;
      }
      nCount++;
 }
-void insertLast(DCLL::node **head,DCLL::node **tail, string name, int age){
-     if(*head == nullptr || *tail == nullptr){
-          insertFirst(head,tail,name,age);
+void insertLast(DCLL::node **head, DCLL::node **tail, string name, int age)
+{
+     if (*head == nullptr || *tail == nullptr)
+     {
+          insertFirst(head, tail, name, age);
           return;
      }
      DCLL::node *nn = DCLL::createNode();
@@ -69,21 +76,26 @@ void insertLast(DCLL::node **head,DCLL::node **tail, string name, int age){
      nn->next = *head;
      nCount++;
 }
-void insertPos(DCLL::node **head,DCLL::node **tail, string name, int age, int pos){
-     if(pos == 1){
-          insertFirst(head,tail,name,age);
+void insertPos(DCLL::node **head, DCLL::node **tail, string name, int age, int pos)
+{
+     if (pos < 1 || pos > nCount + 1)
+     {
+          cout << "Invalid Position!";
           return;
      }
-     if(pos < 1 || pos > nCount + 1){
-          cout<<"Invalid Position!";
+     if (pos == 1)
+     {
+          insertFirst(head, tail, name, age);
           return;
      }
-     if(pos == nCount + 1){
-          insertLast(head,tail,name,age);
+     if (pos == nCount + 1)
+     {
+          insertLast(head, tail, name, age);
           return;
      }
      DCLL::node *temp = *head;
-     for(int i = 1; i < pos - 1; i++){
+     for (int i = 1; i < pos - 1; i++)
+     {
           temp = temp->next;
      }
      DCLL::node *nn = DCLL::createNode();
@@ -95,42 +107,126 @@ void insertPos(DCLL::node **head,DCLL::node **tail, string name, int age, int po
      temp->next = nn;
      nCount++;
 }
-// void deleteFirst(DCLL::node **head, string name, int age);
-// void deleteLast(DCLL::node **head, string name, int age);
-// void deletePos(DCLL::node **head, string name, int age, int pos);
-// void updateFirst(DCLL::node **head, string name, int age);
-// void updateLast(DCLL::node **head, string name, int age);
-// void updatePos(DCLL::node **head, string name, int age, int pos);
-// void sort(DCLL::node **head);
-// void search(DCLL::node **head, string name); // Function Overloading
-// void search(DCLL::node **head, int age);     // Function Overloading
-// void search(DCLL::node **head);     // Function OVerloading
-void display(DCLL::node **head,DCLL::node **tail){
+void deleteFirst(DCLL::node **head, DCLL::node **tail)
+{
+     if (*head == nullptr || *tail == nullptr)
+     {
+          return;
+     }
+     if((*head)->next == *tail){
+          delete *head;
+          *head = nullptr;
+          nCount--;
+          return;
+     }
+     (*head)->next->prev = *tail;
+     (*tail)->next = (*head)->next;
+     delete *head;
+     *head = (*tail)->next;
+     nCount--;
+}
+void deleteLast(DCLL::node **head,DCLL::node **tail){
      if(*head == nullptr){
-          cerr<<"The list is empty!";
+          return;
+     }
+     if((*head)->next == *head){
+          delete *head;
+          *head = nullptr;
+          nCount--;
+          return;
+     }
+     DCLL::node *temp = *tail;
+     *tail = (*tail)->prev;
+     (*tail)->next = *head;
+     (*head)->prev = *tail;
+     delete temp;
+     temp = nullptr;
+     nCount--;
+}
+void deletePos(DCLL::node **head,DCLL::node **tail, int pos){
+     if(pos < 1 || pos > nCount){
+          cout<<"Invalid Position!";
+          return;
+     }
+     if(pos == 1){
+          deleteFirst(head,tail);
+          return;
+     }
+     if(pos == nCount){
+          deleteLast(head,tail);
           return;
      }
      DCLL::node *temp = *head;
-     cout<<"Head->";
-     while(true){
-          cout<<"("<<temp->user->name<<"-"<<temp->user->age<<")->";
+     for(int i = 1; i < pos - 1; i++){
           temp = temp->next;
-          if(temp == *head) break;
      }
-     cout<<"Tail"<<endl;
-     cout<<"Nodes : "<<nCount;
+     DCLL::node *temp1 = temp->next->next;
+     delete temp->next;
+     temp->next = temp1;
+     temp1->prev = temp;
+     nCount--;
+}
+// void update(DCLL::node **head, string name, int age, int pos);
+// void sort(DCLL::node **head);
+// void search(DCLL::node **head, string name); // Function Overloading
+// void search(DCLL::node **head, int age);     // Function Overloading
+void display(DCLL::node **head, DCLL::node **tail)
+{
+     if (*head == nullptr)
+     {
+          cerr << "The list is empty!";
+          return;
+     }
+     DCLL::node *temp = *head;
+     cout << "Head->";
+     while (true)
+     {
+          cout << "(" << temp->user->name << "-" << temp->user->age << ")->";
+          temp = temp->next;
+          if (temp == *head)
+               break;
+     }
+     cout << "Tail" << endl;
+     cout << "Nodes : " << nCount;
 }
 // void displayReverse(DCLL::node **head);
 
 int main()
 {
+     ifstream inputFile("DCLL_input.txt");
+     if(!inputFile.is_open()){
+          cerr<<"Unable to open the input file!";
+          return 1;
+     }
      DCLL::node *head{nullptr};
      DCLL::node *tail{nullptr};
      int choice, pos, age;
      string name;
      cout << endl
-          << "Single Circular LL";
-     while (1)
+          << "Single Circular LL"<<endl;
+     while(inputFile >> choice){
+          switch(choice){
+               case 1:
+                    inputFile >> name >> age;
+                    insertFirst(&head,&tail,name,age);
+                    break;
+               case 2:
+                    inputFile >> name >>age;
+                    insertLast(&head,&tail,name,age);
+                    break;
+               case 3:
+                    inputFile >>pos>> name >> age;
+                    insertPos(&head,&tail,name,age,pos);
+                    break;
+               case 4:
+                    display(&head,&tail);
+                    break;
+               default:
+                    cerr<<"Error taking input from file!";
+                    break;
+          }
+     }
+     while (true)
      {
           cout << endl
                << "1.Insert at First";
@@ -139,23 +235,19 @@ int main()
           cout << endl
                << "3.Insert at Specific Position";
           cout << endl
-               << "4.Insert at First";
+               << "4.Delete at First";
           cout << endl
-               << "5.Insert at Last";
+               << "5.Delete at Last";
           cout << endl
-               << "6.Insert at Specific Position";
+               << "6.Delete at Specific Position";
           cout << endl
-               << "7.Insert at First";
+               << "7.Update at Specific Position";
           cout << endl
-               << "8.Insert at Last";
+               << "8.Sort(Elemental)";
           cout << endl
-               << "9.Insert at Specific Position";
+               << "9.Search(Linear)";
           cout << endl
-               << "10.Sort(Elemental)";
-          cout << endl
-               << "11.Search(Linear)";
-          cout << endl
-               << "12.Display";
+               << "10.Display";
           cout << endl
                << "0.Exit";
           cout << endl
@@ -166,37 +258,55 @@ int main()
           case 1:
                cout << endl
                     << "Enter the User's Name : ";
-               cin>>name;
+               cin >> name;
                cout << endl
                     << "Enter the User's Age : ";
                cin >> age;
                insertFirst(&head, &tail, name, age);
-               display(&head,&tail);
+               display(&head, &tail);
                break;
           case 2:
                cout << endl
                     << "Enter the User's Name : ";
-               cin>>name;
+               cin >> name;
                cout << endl
                     << "Enter the User's Age : ";
                cin >> age;
                insertLast(&head, &tail, name, age);
-               display(&head,&tail);
+               display(&head, &tail);
                break;
           case 3:
-          cout<<"Enter the Position : ";
-          cin >> pos;
+               cout << "Enter the Position : ";
+               cin >> pos;
                cout << endl
-               << "Enter the User's Name : ";
-               cin>>name;
+                    << "Enter the User's Name : ";
+               cin >> name;
                cout << endl
                     << "Enter the User's Age : ";
                cin >> age;
-               insertPos(&head, &tail, name, age,pos);
+               insertPos(&head, &tail, name, age, pos);
+               display(&head, &tail);
+               break;
+          case 4:
+               deleteFirst(&head, &tail);
+               display(&head, &tail);
+               break;
+          case 5:
+               deleteLast(&head,&tail);
+               display(&head,&tail);
+               break;
+          case 6:
+               cout<<"Enter the Position : ";
+               cin>>pos;
+               deletePos(&head,&tail,pos);
+               display(&head,&tail);
+               break;
+          case 10:
                display(&head,&tail);
                break;
           case 0:
-               cout<<endl<<"Exiting...";
+               cout << endl
+                    << "Exiting...";
                return 0;
           default:
                cout << endl

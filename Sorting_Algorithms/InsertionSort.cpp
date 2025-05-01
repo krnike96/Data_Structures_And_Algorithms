27ea07dbdda88a6c76ac8347f1e7da3e25442058
Niket Kumar <krnike96@gmail.com>
2025-05-01 23:22:31 +0530

    InsertionSort Algorithm Implementation

--- Sorting_Algorithms/InsertionSort.cpp
+++ Sorting_Algorithms/InsertionSort.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 using namespace std;
 void insertionSort(int * arr,size_t size){
-    
+    if(!size) return;
+    for(int i = 0; i < size; i++){
+        int key = arr[i];
+        int j = i;
+        while(j > 0 && arr[j - 1] > key){
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = key;
+    }
+}
+void printArray(int * arr, size_t size){
+    if(!size){
+        cout << "Array is Empty."<<endl;
+    }
+    for(int i = 0; i < size; i++){
+        cout << arr[i] <<" ";
+    }
+    return;
 }
 int main(){
-    printf("Enter the Size of Array : ");
+    cout << "Enter the Size of Array : ";
     size_t size;
-    scanf("%zu",&size);
+    cin >> size;
     int * arr = new int[size];
+    cout<<"Enter the Array Elements : "<<endl;
     for(int i = 0; i < size; i++){
-        scanf("%d",&arr[i]);
+        cin >> arr[i];
     }
     insertionSort(arr,size);
     printArray(arr,size);

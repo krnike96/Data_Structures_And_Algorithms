ac5738904d102de1f607d2c7244d2da3d44f3b19
Niket Kumar <krnike96@gmail.com>
2025-07-29 00:06:26 +0530

    Add Reverse Doubly LinkedList and unnecessary .exe files Removal

--- Linked_Lists/DoublyLL_DS_ALGO.c
+++ Linked_Lists/DoublyLL_DS_ALGO.c
@@ -1,34 +1,41 @@
-//Using Double Pointer
+// Using Double Pointer
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef struct node{
+typedef struct node
+{
     int data;
-    struct node * next;
-    struct node * prev;
-}node;
+    struct node *next;
+    struct node *prev;
+} node;
 int nCount = 0;
-node * createNode(){
+node *createNode()
+{
     return (node *)malloc(sizeof(node));
 }
 
-void insertFirst(node **head,int emt){
-    node * nn = createNode();
+void insertFirst(node **head, int emt)
+{
+    node *nn = createNode();
     nn->data = emt;
     nn->next = *head;
-    if(*head != NULL)  (*head)->prev = nn;
+    if (*head != NULL)
+        (*head)->prev = nn;
     *head = nn;
     nn->prev = NULL;
     nCount++;
 }
-void insertLast(node **head,int emt){
-    node * nn = createNode();
-    node * temp = *head;
-    if(*head == NULL){
-        insertFirst(head,emt);
+void insertLast(node **head, int emt)
+{
+    node *nn = createNode();
+    node *temp = *head;
+    if (*head == NULL)
+    {
+        insertFirst(head, emt);
         return;
     }
-    while(temp->next != NULL){
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
     temp->next = nn;
@@ -37,23 +44,28 @@ void insertLast(node **head,int emt){
     nn->data = emt;
     nCount++;
 }
-void insertPos(node **head,int pos,int emt){
-    if(pos <= 0){
+void insertPos(node **head, int pos, int emt)
+{
+    if (pos <= 0)
+    {
         printf("Invalid Position!");
         return;
     }
-    if(pos == 1){
-        insertFirst(head,emt);
+    if (pos == 1)
+    {
+        insertFirst(head, emt);
         return;
     }
-    if(pos > nCount){
+    if (pos > nCount)
+    {
         printf("Invalid Position!");
         return;
     }
-    node * temp = *head;
-    node * nn = createNode();
-    for(int i = 1;i < pos - 1; i++){
-        temp=temp->next;
+    node *temp = *head;
+    node *nn = createNode();
+    for (int i = 1; i < pos - 1; i++)
+    {
+        temp = temp->next;
     }
     nn->data = emt;
     nn->next = temp->next;
@@ -62,83 +74,105 @@ void insertPos(node **head,int pos,int emt){
     temp->next = nn;
     nCount++;
 }
-void deleteFirst(node **head){
-    if(*head == NULL){
+void deleteFirst(node **head)
+{
+    if (*head == NULL)
+    {
         return;
     }
-    node * temp = *head;
-    if(temp->next == NULL){
+    node *temp = *head;
+    if (temp->next == NULL)
+    {
         free(*head);
         *head = NULL;
-    }else{
-    temp->next->prev = NULL;
-    *head = temp->next;
-    free(temp);
-    temp = NULL;
+    }
+    else
+    {
+        temp->next->prev = NULL;
+        *head = temp->next;
+        free(temp);
+        temp = NULL;
     }
     nCount--;
 }
-void deleteLast(node **head){
-    if(*head == NULL){
+void deleteLast(node **head)
+{
+    if (*head == NULL)
+    {
         return;
     }
-    if((*head)->next == NULL){
+    if ((*head)->next == NULL)
+    {
         deleteFirst(head);
         return;
     }
-    node * temp = *head;
-    while(temp->next->next != NULL){
+    node *temp = *head;
+    while (temp->next->next != NULL)
+    {
         temp = temp->next;
     }
     free(temp->next);
     temp->next = NULL;
     nCount--;
 }
-void deletePos(node **head,int pos){
-    if(*head == NULL){
+void deletePos(node **head, int pos)
+{
+    if (*head == NULL)
+    {
         return;
     }
-    if(pos <= 0 || pos > nCount){
+    if (pos <= 0 || pos > nCount)
+    {
         printf("Invalid Position!");
         return;
     }
-    if(pos == 1){
+    if (pos == 1)
+    {
         deleteFirst(head);
         return;
     }
-    if(pos == nCount){
+    if (pos == nCount)
+    {
         deleteLast(head);
         return;
     }
-    node * temp = *head;
-    for(int i = 1;i < pos - 1;i++){
+    node *temp = *head;
+    for (int i = 1; i < pos - 1; i++)
+    {
         temp = temp->next;
     }
-    node * temp1 = *head;
+    node *temp1 = *head;
     temp1 = temp->next->next;
     free(temp->next);
     temp->next = temp1;
     temp1->prev = temp;
     nCount--;
 }
-void updateFirst(node **head,int emt){
-    if(*head == NULL){
+void updateFirst(node **head, int emt)
+{
+    if (*head == NULL)
+    {
         return;
     }
     (*head)->data = emt;
 }
-void updateLast(node **head,int emt){
-    if(*head == NULL){
+void updateLast(node **head, int emt)
+{
+    if (*head == NULL)
+    {
         return;
     }
-    node * temp = *head;
-    while(temp->next != NULL){
+    node *temp = *head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
     temp->data = emt;
 }
-void updatePos(node **head,int pos,int emt){
-    if(*head == NULL){
+void updatePos(node **head, int pos, int emt)
+{
+    if (*head == NULL)
+    {
         return;
     }
     if (pos <= 0)
@@ -148,14 +182,16 @@ void updatePos(node **head,int pos,int emt){
     }
     if (pos == 1)
     {
-        updateFirst(head,emt);
+        updateFirst(head, emt);
         return;
     }
     int currPos = 2;
-    node * temp = *head;
-    while(temp != NULL){
+    node *temp = *head;
+    while (temp != NULL)
+    {
         temp = temp->next;
-        if(pos == currPos){
+        if (pos == currPos)
+        {
             temp->data = emt;
             return;
         }
@@ -163,39 +199,48 @@ void updatePos(node **head,int pos,int emt){
     }
     printf("Invalid Position!");
 }
-void sort(node **head){
-    if(*head == NULL || (*head)->next == NULL){
+void sort(node **head)
+{
+    if (*head == NULL || (*head)->next == NULL)
+    {
         return;
     }
-    node * temp;
-    node * end = NULL;
+    node *temp;
+    node *end = NULL;
     int swap;
-    do{
+    do
+    {
         swap = 0;
         temp = *head;
-        while(temp->next != end){
-        if(temp->data > temp->next->data){
-            temp->data ^= temp->next->data ^= temp->data ^= temp->next->data;
-            swap = 1;
-        }
+        while (temp->next != end)
+        {
+            if (temp->data > temp->next->data)
+            {
+                temp->data ^= temp->next->data ^= temp->data ^= temp->next->data;
+                swap = 1;
+            }
             temp = temp->next;
         }
         end = temp;
-    }while(swap);
+    } while (swap);
 }
 
-void search(node **head){
-    if(*head == NULL){
+void search(node **head)
+{
+    if (*head == NULL)
+    {
         return;
     }
     int key;
     printf("Enter the Key Element : ");
-    scanf("%d",&key);
-    node * temp = *head;
+    scanf("%d", &key);
+    node *temp = *head;
     int pos = 1;
-    while(temp != NULL){
-        if(temp->data == key){
-            printf("The Key Element Found at Position : %d",pos);
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            printf("The Key Element Found at Position : %d", pos);
             return;
         }
         temp = temp->next;
@@ -203,47 +248,79 @@ void search(node **head){
     }
     printf("The Key Element Not Found!");
 }
-void display(node **head){
-    if(*head == NULL){
+
+void reverse(node **head)
+{
+    if (*head == NULL)
+    {
         printf("\nDLL is Empty!");
         return;
     }
-    node * temp = *head;
+    node *curr = *head;
+    node *temp = NULL;
+    while (curr)
+    {
+        temp = curr->prev;
+        curr->prev = curr->next;
+        curr->next = temp;
+
+        temp = curr;
+        curr = curr->prev;
+    }
+    if (temp)
+        *head = temp;
+}
+
+void display(node **head)
+{
+    if (*head == NULL)
+    {
+        printf("\nDLL is Empty!");
+        return;
+    }
+    node *temp = *head;
     printf("#############################################\n\n");
     printf("The DLL is : NULL->");
-    while(temp != NULL){
-        printf("%d->",temp->data);
+    while (temp != NULL)
+    {
+        printf("%d->", temp->data);
         temp = temp->next;
     }
     printf("NULL");
-    printf("\nNodes Count : %d",nCount);
+    printf("\nNodes Count : %d", nCount);
     printf("\n\n#############################################");
 }
-void displayRev(node **head){
-    if(*head == NULL){
+void displayRev(node **head)
+{
+    if (*head == NULL)
+    {
         printf("\nDLL is Empty!");
         return;
     }
-    node * temp = *head;
-    while(temp->next != NULL){
+    node *temp = *head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
     printf("#############################################\n\n");
     printf("The DLL is : NULL");
-    while(temp != NULL){
-        printf("<-%d",temp->data);
+    while (temp != NULL)
+    {
+        printf("<-%d", temp->data);
         temp = temp->prev;
     }
     printf("<-NULL");
-    printf("\nNodes Count : %d",nCount);
+    printf("\nNodes Count : %d", nCount);
     printf("\n\n#############################################");
 }
 
-int main(){
-    node * head = NULL;
-    int choice,pos,emt;
+int main()
+{
+    node *head = NULL;
+    int choice, pos, emt;
     printf("\nDoublyLL");
-    while(1){
+    while (1)
+    {
         printf("\n1.Insert At First");
         printf("\n2.Insert At Last");
         printf("\n3.Insert At Specific Position");
@@ -255,85 +332,91 @@ int main(){
         printf("\n9.Update At Specific Position");
         printf("\n10.Sort(Elemental)");
         printf("\n11.Search(Linear)");
-        printf("\n12.Display");
-        printf("\n13.Display in Reverse");
+        printf("\n12.Reverse(In_Place)");
+        printf("\n13.Display");
+        printf("\n14.Display in Reverse");
         printf("\n0.Exit");
         printf("\nEnter Your Choice : ");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1:
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                insertFirst(&head,emt);
-                display(&head);
-                break;
-            case 2:
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                insertLast(&head,emt);
-                display(&head);
-                break;
-            case 3:
-                printf("Enter the Position : ");
-                scanf("%d",&pos);
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                insertPos(&head,pos,emt);
-                display(&head);
-                break;
-            case 4:
-                deleteFirst(&head);
-                display(&head);
-                break;
-            case 5:
-                deleteLast(&head);
-                display(&head);
-                break;
-            case 6:
-                printf("Enter the Position : ");
-                scanf("%d",&pos);
-                deletePos(&head,pos);
-                display(&head);
-                break;
-            case 7:
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                updateFirst(&head,emt);
-                display(&head);
-                break;
-            case 8:
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                updateLast(&head,emt);
-                display(&head);
-                break;
-            case 9:
-                printf("Enter the Position : ");
-                scanf("%d",&pos);
-                printf("Enter the Element : ");
-                scanf("%d",&emt);
-                updatePos(&head,pos,emt);
-                display(&head);
-                break;
-            case 10: 
-                sort(&head);
-                display(&head);
-                break;
-            case 11:
-                search(&head);
-                break;
-            case 12:
-                display(&head);
-                break;
-            case 13:
-                displayRev(&head);
-                break;
-            case 0:
-                printf("Exiting...");
-                return 0;
-            default:
-                printf("\nError Code : 404 - Not found!");
-                break;
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            insertFirst(&head, emt);
+            display(&head);
+            break;
+        case 2:
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            insertLast(&head, emt);
+            display(&head);
+            break;
+        case 3:
+            printf("Enter the Position : ");
+            scanf("%d", &pos);
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            insertPos(&head, pos, emt);
+            display(&head);
+            break;
+        case 4:
+            deleteFirst(&head);
+            display(&head);
+            break;
+        case 5:
+            deleteLast(&head);
+            display(&head);
+            break;
+        case 6:
+            printf("Enter the Position : ");
+            scanf("%d", &pos);
+            deletePos(&head, pos);
+            display(&head);
+            break;
+        case 7:
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            updateFirst(&head, emt);
+            display(&head);
+            break;
+        case 8:
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            updateLast(&head, emt);
+            display(&head);
+            break;
+        case 9:
+            printf("Enter the Position : ");
+            scanf("%d", &pos);
+            printf("Enter the Element : ");
+            scanf("%d", &emt);
+            updatePos(&head, pos, emt);
+            display(&head);
+            break;
+        case 10:
+            sort(&head);
+            display(&head);
+            break;
+        case 11:
+            search(&head);
+            break;
+        case 12:
+            reverse(&head);
+            display(&head);
+            break;
+        case 13:
+            display(&head);
+            break;
+        case 14:
+            displayRev(&head);
+            break;
+        case 0:
+            printf("Exiting...");
+            return 0;
+        default:
+            printf("\nError Code : 404 - Not found!");
+            break;
         }
     }
     return 0;

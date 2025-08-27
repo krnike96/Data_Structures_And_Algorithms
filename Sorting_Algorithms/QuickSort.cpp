bb8a619c7290545e23a141ba961d458705745616
Niket Kumar <krnike96@gmail.com>
2025-08-28 00:47:19 +0530

    feat: Added an Addon Lomuto Partition in QuickSort

--- Sorting_Algorithms/QuickSort.cpp
+++ Sorting_Algorithms/QuickSort.cpp
@@ -3,9 +3,27 @@
 #include <algorithm>
 using namespace std;
 
+int lomutoPartition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[high];      //Right most element is the Pivot
+    int i = low - 1;            //Index of the smaller element
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)    //check if the current element is smaller than or equal to the pivot
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], pivot);    //Place pivot in correct position
+    return i + 1;               //Return partition index
+}
+
 int hoarePartition(vector<int> &arr, int low, int high)
 {
-    int pivot = arr[low];       //first element is pivot
+    int pivot = arr[low]; // first element is pivot
     int i = low - 1;
     int j = high + 1;
 
@@ -14,26 +32,36 @@ int hoarePartition(vector<int> &arr, int low, int high)
         do
         {
             i++;
-        } while (arr[i] < pivot); //finding element on left that should be on right
+        } while (arr[i] < pivot); // finding element on left that should be on right
         do
         {
             j--;
-        } while (arr[j] > pivot); //finding element on right that should be on left
+        } while (arr[j] > pivot); // finding element on right that should be on left
 
         if (i >= j)
-            return j;             //if both pointers met
+            return j; // if both pointers met
 
         swap(arr[i], arr[j]);
     }
 }
 
-void quickSort(vector<int> &arr, int low, int high)
+void quickSortHoare(vector<int> &arr, int low, int high)
 {
     if (low < high)
     {
         int pivot = hoarePartition(arr, low, high);
-        quickSort(arr, low, pivot);         //pivot included on left
-        quickSort(arr, pivot + 1, high);
+        quickSortHoare(arr, low, pivot); // pivot included on left
+        quickSortHoare(arr, pivot + 1, high);
+    }
+}
+
+void quickSortLomuto(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = lomutoPartition(arr, low, high);
+        quickSortLomuto(arr, low, pivot - 1); // Not including Pivot in either parts
+        quickSortLomuto(arr, pivot + 1, high);
     }
 }
 
@@ -61,7 +89,14 @@ int main()
         cin >> arr[i];
     }
 
-    quickSort(arr, 0, size - 1);
+    cout << "1.Use Lomuto Partition\n2.Use Hoare Partition\nEnter your Choice : ";
+    int ch;
+    cin >> ch;
+    if (2 == ch)
+        quickSortLomuto(arr, 0, size - 1);
+    else
+        quickSortHoare(arr, 0, size - 1);
+
     printArr(arr);
 
     return 0;
